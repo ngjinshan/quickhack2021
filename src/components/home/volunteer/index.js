3bc55ed521fdfc4d2959f625906a7a440f93ffe5
Wai Hao Chan <wcha0041@student.monash.edu>
2021-07-18 01:20:34 +0800

    form alignment issues

--- src/components/home/volunteer/index.js
+++ src/components/home/volunteer/index.js
@@ -13,73 +13,76 @@ const Volunteer = () => {
             <p>
                 Fill up the form below and we will contact you as soon as possible!
             </p>
-            <Form>
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>Name: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Control className="input" type="text"/>
-                    </Col>
-                </Form.Group>
+            <div className="container">
+                <Form>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Name: </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control className="input" type="text"/>
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>NRIC No.: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Control type="text"/>
-                    </Col>
-                </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>NRIC No.: </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="text"/>
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row}>
-                    <Form.Label as="legend" column sm={2}>Gender: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Check 
-                            label="Male"
-                            inline
-                            name="gender"
-                            type="radio"
-                        />
-                        <Form.Check
-                            label="Female"
-                            inline
-                            name="gender"
-                            type="radio"
-                        />
-                        <Form.Check
-                            label="Prefer Not to Say"
-                            inline
-                            name="gender"
-                            type="radio"
-                        />  
-                    </Col>
-                </Form.Group>
-                
+                    <Form.Group as={Row}>
+                        <Form.Label as="legend" column sm={2}>Gender: </Form.Label>
+                        <Col sm={9} style={{display:"flex"}}>
+                            <Form.Check 
+                                label="Male"
+                                inline
+                                name="gender"
+                                type="radio"
+                            />
+                            <Form.Check
+                                label="Female"
+                                inline
+                                name="gender"
+                                type="radio"
+                            />
+                            <Form.Check
+                                label="Prefer Not to Say"
+                                inline
+                                name="gender"
+                                type="radio"
+                            />  
+                        </Col>
+                    </Form.Group>
+                    
 
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>Address: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Control className="input" type="text"/>
-                    </Col>
-                </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Address: </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control className="input" type="text"/>
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row}>
-                    <Form.Label column sm ={2}>Email: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Control type="text"/>
-                    </Col>
-                </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm ={2}>Email: </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="text"/>
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>Contact No.: </Form.Label>
-                    <Col sm={9}>
-                        <Form.Control type="tel"/>
-                    </Col>
-                </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Contact No.: </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control type="tel"/>
+                        </Col>
+                    </Form.Group>
 
-                <Form.Group as={Row}>
-                    <Col sm={{span:10, offset:1}}>
-                        <Button type="submit">Submit</Button>
-                    </Col>
-                </Form.Group>
-            </Form>
+                    <Form.Group as={Row}>
+                        <Col sm={{span:10, offset:1}}>
+                            <Button type="submit">Submit</Button>
+                        </Col>
+                    </Form.Group>
+                </Form>
+            </div>
+            
         </div>
     )
 }
